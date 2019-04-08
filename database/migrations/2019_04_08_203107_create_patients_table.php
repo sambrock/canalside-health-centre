eb52fc278479ad07c3db7ff4a476196d260de216
Sam Brocklehurst <sambrocklehurst1998@gmail.com>
2019-04-08 23:39:20 +0100

    Added patients migration.

---
+++ database/migrations/2019_04_08_203107_create_patients_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePatientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+          Schema::create('patients', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('firstname', 50);
+            $table->string('lastname', 50);
+            $table->string('address', 100);
+            $table->string('postcode', 9);
+            $table->decimal('mobile_number', 11, 0);
+            $table->string('gender', 6);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('patients');
+    }
+}
