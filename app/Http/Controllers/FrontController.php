40a4b830d8b57ee7aad5e32bbc6429a4d0ae9092
Sam Brocklehurst <sambrocklehurst1998@gmail.com>
2019-04-10 00:13:28 +0100

    Added validation.

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -12,4 +12,41 @@ class FrontController extends Controller
         $patients = Patient::all();
         return view('chc/list-view', ['patients' => $patients]);
     }
+    function details($patientId)
+    {
+        $patient = Patient::find($patientId);
+        return view('chc/details-view', ['patient' => $patient]);
+    }
+    function register()
+    {
+        return view('chc/register-view');
+    }
+    function save(Request $request)
+    {
+        $this->validate($request, [
+            'firstname' => 'required|max:100',
+            'lastname' => 'required|max:100',
+            'address' => 'required|max:300',
+            'postcode' => 'required|max:9',
+        ]);
+        $patient = new Patient();
+        $patient->firstname = $request->firstname;
+        $patient->lastname=$request->lastname;
+        $patient->address=$request->address;
+        $patient->postcode=$request->postcode;
+        $patient->mobile_number=$request->mobile;
+        $patient->gender=$request->gender;
+        $patient->save();
+        return redirect('index');
+    }
+    function deleteList()
+    {
+        $patients = Patient::all();
+        return view('chc/delete-list-view',['patients' => $patients]);
+    }
+    function delete(Request $request)
+    {
+        Patient::destroy($request->patients);
+        return redirect('index');
+    }
 }
