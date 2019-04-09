dba9333e064b4d193f33cdc331c1391ad5190b12
Sam Brocklehurst <sambrocklehurst1998@gmail.com>
2019-04-10 00:13:36 +0100

    Edited master layout.

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,16 +4,25 @@
         <link href="{{asset('/css/style.css')}}" rel="stylesheet" type="text/css">
     </head>
     <body>
-        @section('header')
-            <ul>
-                <li><a href="{{url('list')}}">Read</a></li>
-                <li><a href="{{url('create')}}">Create</a></li>
-                <li><a href="{{url('deletelist')}}">Delete </a></li>
-            </ul>
-        @show
+        <header>
+            <img id="logo" src="{{asset('/img/chc-logo.svg')}}" height="18">
+        </header>
+        @section('sidebar')
+        <main>
+            <div class="sidebar">
+                <span class="sidebar-header">Patient</span>
+                <ul>
+                    <li><a href="{{url('list')}}">Read</a></li>
+                    <li><a href="{{url('register')}}">Register</a></li>
+                    <li><a href="{{url('deletelist')}}">Delete </a></li>
+                </ul>
+            </div>
 
-        <div class="container">
-            @yield('content')
-        </div>
+            @show
+
+            <div class="container">
+                @yield('content')
+            </div>
+        </main>
     </body>
 </html>
