206b86bf1385aac8fd16d94097dde03a3ff14992
Sam Brocklehurst <sambrocklehurst1998@gmail.com>
2019-04-13 01:53:27 +0100

    Added patient list view.

--- resources/views/chc/list-view.blade.php
+++ resources/views/chc/list-view.blade.php
@@ -3,10 +3,36 @@
 @section('title', 'View All Films')
 
 @section('content')
-    <h1>View All Patients</h1>
-@foreach ($patients as $patient)
-        <p>
-            <a href="{{url('details/'.$patient->id)}}">{{$patient->firstname}} {{$patient->lastname}}</a>
-        </p>
-    @endforeach
+<section class="list">
+    <div class="list-header">
+        <h1>Patients</h1>
+        <div class="list-search">
+            <span>Search: </span>
+            <input type="text" placeholder="Patient name...">
+        </div>
+
+    </div>
+    <div class="patinents">
+        <div class="patients-headers">
+            <span class="patients-header-name">Name</span>
+            <span class="patients-header-address">Address</span>
+            <span class="patients-header-mobile">Mobile</span>
+            <span class="patients-header-doctor">Doctor</span>
+        </div>
+        <div class="patients-details">
+            @foreach ($patients as $patient)
+            <div class="patient">
+                <span class="patient-name"><a href="patients/details/{{$patient->id}}">{{$patient->lastname}}, {{$patient->firstname}}</a></span>
+                <span class="patient-address">{{$patient->address}}, {{$patient->postcode}}</span>
+                <span class="patient-mobile">{{$patient->mobile_number}}</span>
+                @foreach ($doctors as $doctor)
+                @if (($doctor->id)===($patient->doctor_id))
+                <span class="patient-doctor">Dr. {{$doctor->firstname}} {{$doctor->lastname}}</span>
+                @endif
+                @endforeach
+            </div>
+            @endforeach
+        </div>
+    </div>
+</section>
 @endsection
