e4883b7f4b7d3000c684b1b1c9dc437b0912471e
Sam Brocklehurst <sambrocklehurst1998@gmail.com>
2019-04-14 23:47:19 +0100

    Added search results count.

--- resources/views/chc/list-view.blade.php
+++ resources/views/chc/list-view.blade.php
@@ -8,7 +8,10 @@
         <h1>Patients</h1>
         <div class="list-search">
             <input type="text" placeholder="Search..." id="patient-search" value="">
-            <span id="patient-count">{{ $patient_count }} / {{ $patient_all_count }}</span>
+            <div class="results-count">
+            <span id="display-count">{{ $patient_count }} / </span><span id="search-count">{{ $patient_all_count }} patients</span>
+            </div>
+
         </div>
 
     </div>
@@ -16,16 +19,18 @@
         <div class="patients-headers">
             <span class="patients-header-name">Name</span>
             <span class="patients-header-address">Address</span>
-            <span class="patients-header-mobile">Mobile</span>
+            <span class="patients-header-dob">DOB</span>
+            <span class="patients-header-gender">Gender</span>
+            <span class="patients-header-mobile">Contact</span>
         </div>
         <div class="patients-details">
             @foreach ($patients as $patient)
             <div class="patient">
                 <span class="patient-name"><a href="patients/details/{{$patient->id}}">{{$patient->lastname}}, {{$patient->firstname}}</a></span>
                 <span class="patient-address">{{$patient->address}}, {{$patient->postcode}}</span>
-                <span class="patient-mobile">{{$patient->mobile_number}}</span>
-                <span class="patient-age">{{$patient->dob}}</span>
+                <span class="patient-dob">{{$patient->dob}}</span>
                 <span class="patient-gender">{{$patient->gender}}</span>
+                <span class="patient-mobile">{{$patient->mobile_number}}</span>
             </div>
             @endforeach
         </div>
