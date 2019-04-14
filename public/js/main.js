d57b6f1693ca0a123e782b34e8baab4fad8149de
Sam Brocklehurst <sambrocklehurst1998@gmail.com>
2019-04-14 23:47:11 +0100

    Added search results count.

--- public/js/main.js
+++ public/js/main.js
@@ -10,10 +10,10 @@ $(function () {
             url: 'name/'+ $searchTerm,
             dataType: "json",
             success: function(data){
+                $('#search-count').text(data.patients.length+" patients");
                 $('.patients-details').empty();
                 $.each(data.patients, function(index, patient) {
-                    console.log(patient.firstname+' '+patient.lastname);
-                    $('.patients-details').append('<div class="patient"><span class="patient-name"><a href="patients/details/'+patient.id+'">'+patient.lastname+', '+patient.firstname+'</a></span><span class="patient-address">'+patient.address+', '+patient.postcode+'</span><span class="patient-mobile">'+patient.mobile_number+'</span></div>');
+                    $('.patients-details').append('<div class="patient"><span class="patient-name"><a href="patients/details/'+patient.id+'">'+patient.lastname+', '+patient.firstname+'</a></span><span class="patient-address">'+patient.address+', '+patient.postcode+'</span><span class="patient-dob">'+patient.dob+'</span><span class="patient-gender">'+patient.gender+'</span><span class="patient-mobile">'+patient.mobile_number+'</span></div>');
                     //$('.patient').append('');
                 });
             }
