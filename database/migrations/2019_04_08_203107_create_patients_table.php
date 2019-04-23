87db0baba386ddb209c251ac580282d7797ac23d
Sam Brocklehurst <sambrocklehurst1998@gmail.com>
2019-04-23 18:51:24 +0100

    Changed data type in migration.

--- database/migrations/2019_04_08_203107_create_patients_table.php
+++ database/migrations/2019_04_08_203107_create_patients_table.php
@@ -19,13 +19,13 @@ class CreatePatientsTable extends Migration
             $table->string('lastname', 50);
             $table->string('address', 100);
             $table->string('postcode', 9);
-            $table->decimal('mobile_number', 11, 0);
+            $table->char('mobile_number', 11);
             $table->string('gender', 6);
             $table->date('dob');
             $table->unsignedInteger('doctor_id');
             $table->timestamps();
         });
-        DB::statement("ALTER TABLE patients AUTO_INCREMENT = 1001;");
+        DB::update("ALTER TABLE patients AUTO_INCREMENT = 1000;");
     }
 
     /**
