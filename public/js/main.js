a8e697967b7945df29032030bda09f2bb86db7eb
Sam Brocklehurst <sambrocklehurst1998@gmail.com>
2019-04-24 18:51:34 +0100

    Added error message for no search results.

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,5 @@
 $(function () {
     $('#patient-search').keyup(function() {
-
         if($(this).val().length > 1){
             $searchTerm = $(this).val();
             $(".patient").hide();
@@ -15,16 +14,21 @@ $(function () {
             url: 'patients/'+ $searchTerm,
             dataType: "json",
             success: function(data){
-                $('#search-count').text(data.patients.length+" patients");
                 $('.patient-result').remove();
                 if($searchTerm != "none"){
                     $("#display-count").html(data.patients.length);
                 }else{
                     $("#display-count").html("55");
                 }
-                $.each(data.patients, function(index, patient) {
-                    $('.patients-details').prepend('<div class="patient-result"><span class="patient-name"><a href="details/'+patient.id+'">'+patient.firstname+' '+patient.lastname+'</a></span><span class="patient-address">'+patient.address+', '+patient.postcode+'</span><span class="patient-dob">'+patient.dob+'</span><span class="patient-gender">'+patient.gender+'</span><span class="patient-mobile">'+patient.mobile_number+'</span></div>');
-                });
+                if(data.patients.length != 0){
+                    $.each(data.patients, function(index, patient) {
+                        $('.patients-details').prepend('<div class="patient-result"><span class="patient-name"><a href="details/'+patient.id+'">'+patient.firstname+' '+patient.lastname+'</a></span><span class="patient-address">'+patient.address+', '+patient.postcode+'</span><span class="patient-dob">'+patient.dob+'</span><span class="patient-gender">'+patient.gender+'</span><span class="patient-mobile">'+patient.mobile_number+'</span></div>');
+                    });
+                }else{
+                    if($('#patient-search').val().length > 1){
+                        $('.patients-details').prepend('<div class="patient-result"><span class="patient-error">No patients found</span>');
+                    }
+                }
             }
         });
     });
