ee03116ab352861cf6296fde0ed516652dacdd86
Sam Brocklehurst <sambrocklehurst1998@gmail.com>
2019-04-24 01:23:05 +0100

    Changed url to patients.

--- public/js/main.js
+++ public/js/main.js
@@ -1,17 +1,18 @@
 $(function () {
     $('#patient-search').keyup(function() {
-        if($(this).val().length){
+        if($(this).val().length > 1){
             $searchTerm = $(this).val();
         }else{
             $searchTerm = "none";
         }
         $.ajax({
             method: 'get',
-            url: 'name/'+ $searchTerm,
+            url: 'patients/'+ $searchTerm,
             dataType: "json",
             success: function(data){
                 $('#search-count').text(data.patients.length+" patients");
                 $('.patients-details').empty();
+                $("#display-count").html(data.patients.length);
                 $.each(data.patients, function(index, patient) {
                     $('.patients-details').append('<div class="patient"><span class="patient-name"><a href="patients/details/'+patient.id+'">'+patient.firstname+' '+patient.lastname+'</a></span><span class="patient-address">'+patient.address+', '+patient.postcode+'</span><span class="patient-dob">'+patient.dob+'</span><span class="patient-gender">'+patient.gender+'</span><span class="patient-mobile">'+patient.mobile_number+'</span></div>');
                     //$('.patient').append('');
@@ -21,3 +22,8 @@ $(function () {
     });
 });
 
+$(".patient").click(function(){
+    $link = $(this).find("a").attr("href")
+    console.log($link);
+    window.location = $link;
+})
