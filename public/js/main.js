bd00ba68be7f88ebfb69a3009ac3866390bd6fa5
Sam Brocklehurst <sambrocklehurst1998@gmail.com>
2019-04-24 18:23:41 +0100

    Changed search function to hide patients rather than empty.

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,14 @@
 $(function () {
     $('#patient-search').keyup(function() {
+
         if($(this).val().length > 1){
             $searchTerm = $(this).val();
+            $(".patient").hide();
+            $(".pagination").hide();
         }else{
             $searchTerm = "none";
+            $(".patient").show();
+            $(".pagination").show();
         }
         $.ajax({
             method: 'get',
@@ -11,19 +16,22 @@ $(function () {
             dataType: "json",
             success: function(data){
                 $('#search-count').text(data.patients.length+" patients");
-                $('.patients-details').empty();
-                $("#display-count").html(data.patients.length);
+                $('.patient-result').remove();
+                if($searchTerm != "none"){
+                    $("#display-count").html(data.patients.length);
+                }else{
+                    $("#display-count").html("55");
+                }
                 $.each(data.patients, function(index, patient) {
-                    $('.patients-details').append('<div class="patient"><span class="patient-name"><a href="patients/details/'+patient.id+'">'+patient.firstname+' '+patient.lastname+'</a></span><span class="patient-address">'+patient.address+', '+patient.postcode+'</span><span class="patient-dob">'+patient.dob+'</span><span class="patient-gender">'+patient.gender+'</span><span class="patient-mobile">'+patient.mobile_number+'</span></div>');
-                    //$('.patient').append('');
+                    $('.patients-details').prepend('<div class="patient-result"><span class="patient-name"><a href="details/'+patient.id+'">'+patient.firstname+' '+patient.lastname+'</a></span><span class="patient-address">'+patient.address+', '+patient.postcode+'</span><span class="patient-dob">'+patient.dob+'</span><span class="patient-gender">'+patient.gender+'</span><span class="patient-mobile">'+patient.mobile_number+'</span></div>');
                 });
             }
         });
     });
 });
 
-$(".patient").click(function(){
+$(document).on('click', '.patient',(function(){
     $link = $(this).find("a").attr("href")
     console.log($link);
     window.location = $link;
-})
+}))
