ff81d5ff4d711f54405280be28ae41de26c7918a
Sam Brocklehurst <sambrocklehurst1998@gmail.com>
2019-04-24 01:22:18 +0100

    Added update and delete functions.

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Patient;
 use App\Doctor;
+use DateTime;
 
 class FrontController extends Controller
 {
@@ -23,11 +24,9 @@ class FrontController extends Controller
         {
             $patients = Patient::where('firstname', 'like', '%'.$searchTerm.'%')
                 ->orwhere('lastname', 'like', '%'.$searchTerm.'%')
-                ->orwhere('mobile_number', 'like', '%'.$searchTerm.'%')
-                ->orwhere('address', 'like', '%'.$searchTerm.'%')
-                ->orwhere('postcode', 'like', '%'.$searchTerm.'%')
                 ->orderBy('lastname')
                 ->get();
+            $results_count = count($patients);
             return response()->json(['patients' => $patients]);
         }
         if($searchTerm = "none")
@@ -38,15 +37,31 @@ class FrontController extends Controller
     }
     function details($patientId)
     {
-        $patient = Patient::find($patientId);
-        return view('chc/details-view', ['patient' => $patient]);
+        $patient = Patient::where("patients.id", $patientId)
+            ->select('patients.*', 'doctors.firstname as doctor_firstname', 'doctors.lastname as doctor_lastname')
+            ->join('doctors', 'doctors.id', '=', 'patients.doctor_id')
+            ->first();
+        $from = new DateTime($patient->dob);
+        $to   = new DateTime('today');
+        $age = $from->diff($to)->y;
+        return view('chc/details-view', ['patient' => $patient], ['age' => $age]);
     }
     function register()
     {
         $doctors = Doctor::all();
         return view('chc/register-view', ['doctors' => $doctors]);
     }
-    function save(Request $request)
+    function update($patientId)
+    {
+        $doctors = Doctor::all();
+        $patient = Patient::where("patients.id", $patientId)
+            ->select('patients.*', 'patients.id as patient_id', 'doctors.firstname as doctor_firstname', 'doctors.lastname as doctor_lastname')
+            ->join('doctors', 'doctors.id', '=', 'patients.doctor_id')
+            ->first();
+        $day = $patient->dob->format("d");
+        return view('chc/update-view', ['patient' => $patient], ['doctors' => $doctors])->with($day);
+    }
+    function save(Request $request, $action, $patientId)
     {
         $this->validate($request, [
             'firstname' => 'required|regex:/^[a-zA-Z ]+$/|max:50',
@@ -61,7 +76,14 @@ class FrontController extends Controller
             'doctor' => 'required|min:1',
         ]);
 
-        $patient = new Patient();
+        if($action === "register")
+        {
+            $patient = new Patient();
+        }
+        elseif($action === "update")
+        {
+            $patient = Patient::find($patientId);
+        }
         $patient->firstname = $request->firstname;
         $patient->lastname=$request->lastname;
         $patient->address=$request->address;
@@ -75,16 +97,19 @@ class FrontController extends Controller
         $patient->dob=$dob;
         $patient->doctor_id=$request->doctor;
         $patient->save();
+        if($action === "register")
+        {
+            return redirect('details'.'/'.$patient->id);
+        }
+        elseif($action === "update")
+        {
+            return redirect('details'.'/'.$patientId);
+        }
+    }
+    function delete($patientId)
+    {
+        $patient = Patient::find($patientId);
+        $patient->delete();
         return redirect('patients');
-    }
-    function deleteList()
-    {
-        $patients = Patient::all();
-        return view('chc/delete-list-view',['patients' => $patients]);
-    }
-    function delete(Request $request)
-    {
-        Patient::destroy($request->patients);
-        return redirect('index');
     }
 }
