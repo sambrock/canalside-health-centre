d5fbe2059d6a67373d856c7262c0788ba40c6706
Sam Brocklehurst <sambrocklehurst1998@gmail.com>
2019-04-24 01:23:31 +0100

    Added update and delete buttons.

--- resources/views/chc/details-view.blade.php
+++ resources/views/chc/details-view.blade.php
@@ -1,10 +1,43 @@
 @extends('layouts.master')
 @section('title', 'Film details')
 @section('content')
-<h1>{{$patient->firstname}} {{$patient->lastname}}</h1>
-<ul>
-    <li>Gender: {{$patient->gender}}</li>
-    <li>Address: {{$patient->address}}</li>
-    <li>Mobile number: {{$patient->mobile_number}}</li>
-</ul>
+<div class="details-header">
+    <h1>Details</h1>
+    <a id="update-btn" href="{{ url('/') }}/update/{{ $patient->id }}">Update</a>
+    <a id="delete-btn" href="{{ url('/') }}/delete/{{ $patient->id }}">Delete</a>
+</div>
+
+<div class="details-container">
+    <section class="details-patient">
+        <div class="details-patient-name">
+            <h2>{{$patient->firstname}} {{$patient->lastname}}</h2>
+        </div>
+        <div class="details-gender">
+            <span class="details-label">Gender:</span>
+            <span>{{$patient->gender}}</span>
+        </div>
+        <div class="details-age">
+            <span class="details-label">Age:</span>
+            <span>{{ $age }}</span>
+        </div>
+        <div class="details-dob">
+            <span class="details-label">DOB:</span>
+            <span>{{ $patient->dob->format('d/m/Y') }}</span>
+        </div>
+        <div class="details-contact">
+            <span class="details-label">Contact:</span>
+            <span>{{$patient->mobile_number}}</span>
+        </div>
+        <div class="details-address">
+            <span class="details-label">Address:</span>
+            <span>{{$patient->address}}, {{$patient->postcode}}</span>
+        </div>
+        <div class="details-doctor">
+            <span class="details-label">Doctor:</span>
+            <span>Dr. {{$patient->doctor_firstname}} {{$patient->doctor_lastname}}</span>
+        </div>
+    </section>
+    <section class="details-appointment">
+    </section>
+</div>
 @endsection
