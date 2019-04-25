ff94e428dd522736fd8e2763888f41b6fd321623
Sam Brocklehurst <sambrocklehurst1998@gmail.com>
2019-04-25 17:39:27 +0100

    Implemented booking form.

--- resources/views/chc/book-view.blade.php
+++ resources/views/chc/book-view.blade.php
@@ -13,8 +13,10 @@
                     <input type="text" id="patient_id" name="patient_id" value="{{old('patient_id')}}">
                     <span class="form-error">{{$errors->first('patient_id')}}</span>
                 </div>
-                <div>
-                    <span>Firstname</span> <span>lastname</span>
+                <div class="book-patient-details">
+                    <span id="book-patient-name"></span>
+                    <span id="book-patient-address"></span>
+                    <span id="book-patient-contact"></span>
                 </div>
             </div>
             <div class="form-control ">
@@ -22,13 +24,13 @@
                 </label>
                 <div id="dob-controls">
                     <div class="dob-control {{$errors->has('day') ? 'error' : ''}}">
-                        <input type="text" id="day" name="day" placeholder="DD" maxlength="2" value="{{old('day')}}">
+                        <input type="text" id="day" name="day" placeholder="DD" maxlength="2" value="{{ Carbon\Carbon::now()->day }}">
                     </div>
                     <div class="dob-control {{$errors->has('month') ? 'error' : ''}}">
-                        <input type="text" id="month" name="month" placeholder="MM" maxlength="2" value="{{old('month')}}">
+                        <input type="text" id="month" name="month" placeholder="MM" maxlength="2" value="{{ Carbon\Carbon::now()->month }}">
                     </div>
                     <div class="dob-control {{$errors->has('year') ? 'error' : ''}}">
-                        <input type="text" id="year" name="year" placeholder="YYYY" maxlength="4" value="{{old('year')}}">
+                        <input type="text" id="year" name="year" placeholder="YYYY" maxlength="4" value="{{ Carbon\Carbon::now()->year }}">
                     </div>
                 </div>
                 <span class="form-error">{{$errors->first('day')}}</span>
@@ -38,39 +40,32 @@
             <div class="form-control-time">
                 <div class="form-control {{$errors->has('start') ? 'error' : ''}}">
                     <label for="start">Start:</label>
-                    <input type="time" value="13:00" step="900" name="start">
+                    <input type="time" value="13:00" step="900" name="start" id="start-time" min="09:00" max="13:00">
                     <span class="form-error">{{$errors->first('start')}}</span>
                 </div>
                 <div class="form-control {{$errors->has('end') ? 'error' : ''}}">
                     <label for="end">End:</label>
-                    <input type="time" value="13:00" step="900" name="end">
+                    <input type="time" value="13:00" step="900" name="end" id="end-time" min="13:00" max="17:00">
                     <span class="form-error">{{$errors->first('end')}}</span>
                 </div>
             </div>
-
-            <div class="form-control {{$errors->has('postcode') ? 'error' : ''}}">
-                <label for="postcode">Postcode:</label>
-                <input type="text" id="postcode" name="postcode" value="{{old('postcode')}}">
-                <span class="form-error">{{$errors->first('postcode')}}</span>
-            </div>
-            <div class="form-control {{$errors->has('mobile') ? 'error' : ''}}" >
-                <label for="mobile">Mobile number:</label>
-                <input type="text" id="mobile" name="mobile" value="{{old('mobile')}}">
-                <span class="form-error">{{$errors->first('mobile')}}</span>
-            </div>
-
-            <div class="form-control">
-                <label for="gender">Gender:</label>
-                <div class="gender-controls {{$errors->has('gender') ? 'error' : ''}}">
-                    <label><input type="radio" name="gender" value="male" class="form-radio">Male</label>
-                    <label><input type="radio" name="gender" value="female" class="form-radio">Female</label>
-                    <label><input type="radio" name="gender" value="other" class="form-radio">Other</label>
-                </div>
-                <span class="form-error">{{$errors->first('gender')}}</span>
+            <div class="form-control {{$errors->has('notes') ? 'error' : ''}}" id="form-notes">
+                <label for="notes">Notes:</label>
+                <textarea id="notes-txtarea" name="notes" rows="7" cols="50" maxlength='500'>{{old('notes')}}</textarea>
+                <span class="form-error">{{$errors->first('notes')}}</span>
+                <span id="char-count">500</span>
             </div>
         </div>
-        <h3>Doctor details</h3>
-        <input id="register-btn" type="submit" name="submitBtn" value="Register">
+        <div class="form-control {{$errors->has('doctor') ? 'error' : ''}}" id="form-book-doctor">
+            <label for="doctor">Doctor:</label>
+            <select name="doctor" id="book-doctor-select" >
+                @foreach ($doctors as $doctor)
+                <option value='{{$doctor->id}}'>{{$doctor->firstname}} {{$doctor->lastname}}</option>
+                @endforeach
+            </select>
+            <span class="form-error">{{$errors->first('doctor')}}</span>
+        </div>
+        <input id="book-btn" type="submit" name="submitBtn" value="Book">
     </form>
 </section>
 @endsection
