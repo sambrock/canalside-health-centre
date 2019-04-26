70868ee78c419a38b9bf2bac2981072ad9475754
Sam Brocklehurst <sambrocklehurst1998@gmail.com>
2019-04-26 19:35:42 +0100

    Added authentication to all routes.

--- routes/web.php
+++ routes/web.php
@@ -14,18 +14,19 @@ Auth::routes();
 
 Route::get('/', 'FrontController@index');
 
-Route::get('login','FrontController@login');
+Route::get('login','FrontController@login')->name('login');
 Route::post('login/check','FrontController@checkLogin');
+Route::get('logout','FrontController@logout');
 
-Route::get('patients', 'FrontController@list');
-Route::get('patients/{searchTerm}', 'FrontController@searchNames');
-Route::get('details/{patientId}','FrontController@details');
-Route::get('register','FrontController@register');
-Route::post('save/{action}/{patientId}','FrontController@save');
-Route::get('update/{patientId}','FrontController@update');
-Route::get('delete/{patientId}','FrontController@delete');
-Route::get('book','FrontController@book');
-Route::get('book/{patientId}','FrontController@patientBookDetails');
-Route::post('book-appointment','FrontController@bookAppointment');
-Route::post('check-availability','FrontController@checkAvailability');
+Route::get('patients', 'FrontController@list')->middleware('auth');
+Route::get('patients/{searchTerm}', 'FrontController@searchNames')->middleware('auth');
+Route::get('details/{patientId}','FrontController@details')->middleware('auth');
+Route::get('register','FrontController@register')->middleware('auth');
+Route::post('save/{action}/{patientId}','FrontController@save')->middleware('auth');
+Route::get('update/{patientId}','FrontController@update')->middleware('auth');
+Route::get('delete/{patientId}','FrontController@delete')->middleware('auth');
+Route::get('book','FrontController@book')->middleware('auth');
+Route::get('book/{patientId}','FrontController@patientBookDetails')->middleware('auth');
+Route::post('book-appointment','FrontController@bookAppointment')->middleware('auth');
+Route::post('check-availability','FrontController@checkAvailability')->middleware('auth');
 
