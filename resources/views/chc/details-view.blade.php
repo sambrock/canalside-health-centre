12733f4042b58bf0be98705e09583dad78ea326d
Sam Brocklehurst <sambrocklehurst1998@gmail.com>
2019-04-27 00:18:53 +0100

    Added appointments to patient details.

--- resources/views/chc/details-view.blade.php
+++ resources/views/chc/details-view.blade.php
@@ -38,6 +38,45 @@
         </div>
     </section>
     <section class="details-appointment">
+        <div class="appointments">
+            <div class="appointments-headers">
+                <span class="appointments-header-date">Date</span>
+                <span class="appointments-header-start">Start</span>
+                <span class="appointments-header-end">End</span>
+                <span class="appointments-header-notes">Notes</span>
+                <span class="appointments-header-status">Status</span>
+            </div>
+            <div class="appointments-details">
+                @foreach ($appointments as $appointment)
+                <div class="appointment">
+                    <span class="appointment-date">{{$appointment->date}}</span>
+                    <span class="appointment-start">{{\Carbon\Carbon::parse($appointment->start)->format('H:i')}}</span>
+                    <span class="appointment-end">{{\Carbon\Carbon::parse($appointment->end)->format('H:i')}}</span>
+                    <span class="appointment-notes">{{$appointment->notes}}</span>
+                    <div class="appointment-status">
+                        @if($appointment->status === 1)
+                        <span class="appointment-due">Due</span>
+                        @elseif($appointment->status === 2)
+                        <span class="appointment-in-progress">In progress</span>
+                        @elseif($appointment->status === 3)
+                        <span class="appointment-complete">Complete</span>
+
+                        @endif
+                    </div>
+                    @if($appointment->status === 1)
+                    <a href="{{url('cancel')}}/{{$appointment->id}} " id="cancel-btn">cancel</a>
+                    @endif
+                </div>
+                @endforeach
+            </div>
+        </div>
+        <div class="pagination-container">
+            {!! $appointments->links(); !!}
+            <div class="results-count">
+                Results: <span id="display-count">{{ $appointments->total() }}</span>
+            </div>
+        </div>
+
     </section>
 </div>
 @endsection
