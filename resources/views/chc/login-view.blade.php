58fe349226ac08aaedac266184c344320301a9f0
Sam Brocklehurst <sambrocklehurst1998@gmail.com>
2019-04-26 19:35:26 +0100

    Hidden links on log in page.

--- resources/views/chc/login-view.blade.php
+++ resources/views/chc/login-view.blade.php
@@ -1,22 +1,24 @@
 @extends('layouts.master')
 @section('title', 'Login')
+@section('sidebar')
+@stop
 @section('content')
 
 <h1>Login</h1>
-<form class="form-horizontal" method="POST" action="{{ url('login/check') }}">
+<form id="login-form" method="POST" action="{{ url('login/check') }}">
     {{ csrf_field() }}
-    <div class="form-control{{ $errors->has('id') ? ' has-error' : '' }}">
+    <div class="form-control {{ $errors->has('id') ? ' has-error' : '' }}">
         <label for="id" class="col-md-4 control-label">User ID</label>
         <input id="user-id" type="text" name="id" value="{{ old('id') }}" required>
         @if ($errors->has('email'))
-            <strong>{{ $errors->first('id') }}</strong>
+        <strong>{{ $errors->first('id') }}</strong>
         @endif
     </div>
-    <div class="form-control{{ $errors->has('password') ? ' has-error' : '' }}">
+    <div class="form-control {{ $errors->has('password') ? ' has-error' : '' }}">
         <label for="password">Password</label>
         <input id="password" type="password" name="password" required>
         @if ($errors->has('password'))
-            <strong>{{ $errors->first('password') }}</strong>
+        <strong>{{ $errors->first('password') }}</strong>
         @endif
     </div>
     <div class="form-control">
@@ -25,7 +27,6 @@
         </button>
     </div>
 </form>
-
 
 
 @endsection
