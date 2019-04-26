af7625a2d39415da879623952ad5d184117fb2de
Sam Brocklehurst <sambrocklehurst1998@gmail.com>
2019-04-26 18:19:25 +0100

    Added role to users table.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,11 +16,12 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('email')->unique();
             $table->string('password');
+            $table->string('role');
             $table->rememberToken();
             $table->timestamps();
         });
+        DB::update("ALTER TABLE users AUTO_INCREMENT = 101;");
     }
 
     /**
